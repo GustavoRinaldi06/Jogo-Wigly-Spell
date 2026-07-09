8c52a16d46fc6c1ddbad2c157aa81ac0506e92f1
Mattelis <mattiastelis@gmail.com>
2026-07-09 16:49:06 -0300

    Cauldron.h

---
+++ include/Cauldron.h
@@ -0,0 +1,31 @@
+#ifndef CAULDRON_H
+#define CAULDRON_H
+
+#include "Component.h"
+#include "Rect_Vec2.h"
+#include "Timer.h"
+#include "Sound.h"
+#include "Bullet.h"
+#include "Animator.h"
+#include <queue>
+#include <memory>
+
+class Cauldron : public Component
+{
+public:
+
+    Cauldron(GameObject &associated, const std::string &spritePath, Animator *anim);
+    ~Cauldron();
+
+    void Start() override;
+    void Update(float dt) override;
+    void Render() override;
+    bool Is(const std::string &type) override;
+    // Colisão
+    GameObject *GetGameObject() { return &associated; }
+    Animator *Playeranim;
+    float distance = 0;
+
+};
+
+#endif
